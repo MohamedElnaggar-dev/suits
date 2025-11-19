286f3552dbc5c686e1e655e6921dc3610c4ea5ca
MohamedElnaggar0 <mohamedelnaggarn@gmail.com>
2025-11-19 07:26:29 +0200

    setup firebase and add some packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:suits_app/core/utils/router/app_routes.dart';
+import 'package:suits_app/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const SuitsApp());
 }
 
