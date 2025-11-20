84e5fa570438807795a1ab6c47e425e24723d86e
MohamedElnaggar0 <mohamedelnaggarn@gmail.com>
2025-11-20 19:52:57 +0200

    create auth repo and create user with email and password method

---
+++ lib/features/auth/domain/repo/auth_repo.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:suits_app/core/utils/error_handeling/failure.dart';
+import 'package:suits_app/features/auth/domain/entities/user_entity.dart';
+
+abstract class AuthRepo {
+  Future<Either<CustomFailure, UserEntity>> createUserwithEmailAndPassword({
+    required String email,
+    required String password,
+    required String name,
+  });
+}
