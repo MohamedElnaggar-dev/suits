8e379125bcdcb35d41a31cc969b44e6d873486ae
MohamedElnaggar0 <mohamedelnaggarn@gmail.com>
2025-11-21 14:31:27 +0200

    create sign in cubit and use case injector

--- lib/core/utils/injector/get_it.dart
+++ lib/core/utils/injector/get_it.dart
@@ -3,6 +3,8 @@ import 'package:suits_app/features/auth/data/data_source/firebase_auth_service.d
 import 'package:suits_app/features/auth/data/repo_impl/auth_repo_impl.dart';
 import 'package:suits_app/features/auth/domain/repo/auth_repo.dart';
 import 'package:suits_app/features/auth/domain/usecase/register_user_use_case.dart';
+import 'package:suits_app/features/auth/domain/usecase/signin_use_case.dart';
+import 'package:suits_app/features/auth/presentation/manger/signin_cubit/signin_cubit.dart';
 import 'package:suits_app/features/auth/presentation/manger/signup_cubit/signup_cubit.dart';
 
 final injector = GetIt.instance;
@@ -24,8 +26,16 @@ Future<void> setupLocator() async {
     () => RegisterUserUseCase(authRepo: injector<AuthRepo>()),
   );
 
+  injector.registerLazySingleton<SigninUseCase>(
+    () => SigninUseCase(authRepo: injector<AuthRepo>()),
+  );
+
   /// Blocs / Cubits
   injector.registerFactory<SignupCubit>(
     () => SignupCubit(injector<RegisterUserUseCase>()),
+  );
+
+  injector.registerFactory<SigninCubit>(
+    () => SigninCubit(injector<SigninUseCase>()),
   );
 }
