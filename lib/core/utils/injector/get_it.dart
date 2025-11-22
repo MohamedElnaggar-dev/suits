1329294b4befbdf6db4b3ef83ecc7f4b8fe8ccf0
MohamedElnaggar0 <mohamedelnaggarn@gmail.com>
2025-11-22 14:29:56 +0200

    create injector to otp (usecase _ cubti _ sercvice)

--- lib/core/utils/injector/get_it.dart
+++ lib/core/utils/injector/get_it.dart
@@ -1,11 +1,16 @@
 import 'package:get_it/get_it.dart';
 import 'package:suits_app/features/auth/data/data_source/firebase_auth_service.dart';
+import 'package:suits_app/features/auth/data/data_source/otp_service.dart';
 import 'package:suits_app/features/auth/data/repo_impl/auth_repo_impl.dart';
+import 'package:suits_app/features/auth/data/repo_impl/otp_repo_impl.dart';
 import 'package:suits_app/features/auth/domain/repo/auth_repo.dart';
+import 'package:suits_app/features/auth/domain/repo/otp_repo.dart';
 import 'package:suits_app/features/auth/domain/usecase/forget_password_usecase.dart';
+import 'package:suits_app/features/auth/domain/usecase/otp_use_case.dart';
 import 'package:suits_app/features/auth/domain/usecase/register_user_use_case.dart';
 import 'package:suits_app/features/auth/domain/usecase/signin_use_case.dart';
 import 'package:suits_app/features/auth/presentation/manger/forget_password_cubit/forget_password_cubit.dart';
+import 'package:suits_app/features/auth/presentation/manger/otp_cubit/otp_cubit.dart';
 import 'package:suits_app/features/auth/presentation/manger/signin_cubit/signin_cubit.dart';
 import 'package:suits_app/features/auth/presentation/manger/signup_cubit/signup_cubit.dart';
 
@@ -18,9 +23,14 @@ Future<void> setupLocator() async {
     () => FirebaseAuthService(),
   );
 
+  injector.registerLazySingleton<OtpService>(() => OtpService());
+
   ///  Repositories
   injector.registerLazySingleton<AuthRepo>(
     () => AuthRepoImpl(injector<FirebaseAuthService>()),
+  );
+  injector.registerLazySingleton<OtpRepo>(
+    () => OtpRepoImpl(otpService: injector<OtpService>()),
   );
 
   /// useCases
@@ -35,6 +45,9 @@ Future<void> setupLocator() async {
   injector.registerLazySingleton<ForgetPasswordUseCase>(
     () => ForgetPasswordUseCase(authRepo: injector<AuthRepo>()),
   );
+  injector.registerLazySingleton<OtpUseCase>(
+    () => OtpUseCase(otpRepo: injector<OtpRepo>()),
+  );
 
   /// Blocs / Cubits
   injector.registerFactory<SignupCubit>(
@@ -48,4 +61,6 @@ Future<void> setupLocator() async {
   injector.registerFactory<ForgetPasswordCubit>(
     () => ForgetPasswordCubit(injector<ForgetPasswordUseCase>()),
   );
+
+  injector.registerFactory<OtpCubit>(() => OtpCubit(injector<OtpUseCase>()));
 }
