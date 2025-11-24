a75663c63d331c3caee14fafe22bd92c0e08c602
MohamedElnaggar0 <mohamedelnaggarn@gmail.com>
2025-11-24 13:06:49 +0200

    add query parameters to get method

--- lib/core/utils/services/api_service.dart
+++ lib/core/utils/services/api_service.dart
@@ -4,7 +4,11 @@ class ApiService {
   final Dio _dio = Dio();
   final String _baseUrl = 'https://api.unsplash.com/';
 
-  Future<void> get({required String endpoint, String? token}) async {
+  Future<void> get({
+    required String endpoint,
+    Map<String, dynamic>? queryParameters,
+    String? token,
+  }) async {
     try {
       final Map<String, String> headers = {};
       if (token != null) {
@@ -12,7 +16,7 @@ class ApiService {
       }
       final response = await _dio.get(
         '$_baseUrl$endpoint',
-
+        queryParameters: queryParameters,
         options: Options(headers: headers),
       );
       if (response.statusCode == 200) {
